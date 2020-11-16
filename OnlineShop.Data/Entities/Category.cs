e3266b58bd3762d8e08e24fec25c651e1655e579
MINHQUAN\ASUS <minhquan.nguyen0511@gmail.com>
2020-11-16 21:39:55 +0700

    create entity

---
+++ OnlineShop.Data/Entities/Category.cs
@@ -0,0 +1,16 @@
+﻿using OnlineShop.Data.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OnlineShop.Data.Entities
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public int SortOrder { get; set; }
+        public bool IsShowOnHome { get; set; }
+        public int? ParentId { get; set; }
+        public Status Status { get; set; }
+    }
+}
